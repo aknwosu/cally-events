0b15cb50297fae8f881788926bde58c495b55b5b
AK Nwosu <akunnarebecca@gmail.com>
2020-10-21 20:21:15 +0100

    Updates

---
+++ precache-manifest.6a4b7e7cbf46f5238fb37625230f8db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeea4d6a8ac34248f44c9d06aa3bffc",
+    "url": "/cally-events/index.html"
+  },
+  {
+    "revision": "f4c47e6044eb0883b99c",
+    "url": "/cally-events/static/css/main.c26464eb.chunk.css"
+  },
+  {
+    "revision": "9e553312069db03e72d8",
+    "url": "/cally-events/static/js/2.daff5a76.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cally-events/static/js/2.daff5a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c47e6044eb0883b99c",
+    "url": "/cally-events/static/js/main.3c508f55.chunk.js"
+  },
+  {
+    "revision": "17bb50c528c2f80435ef",
+    "url": "/cally-events/static/js/runtime-main.0814dbd9.js"
+  }
+]);
