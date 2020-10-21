a9d03af8f90537f8b2da27f55d0c3b5dcb0382b9
AK Nwosu <akunnarebecca@gmail.com>
2020-10-21 20:31:43 +0100

    google verification

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+window.gapi = {};
+gapi = window.gapi
+// gapi.auth2 = {}
+// gapi.client = {}
+// gapi.auth2.getAuthInstance = () => {isSignedIn : {get : () => true, listen; }};
+// gapi.client.init = (v) => true;
+// gapi.load = (a, f) => f();
+gapi.client.request = gapi.client.request || function() {};
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
